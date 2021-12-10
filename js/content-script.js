3f4a76e0fd9962b0d1f0e5757c2de5316833ee47
hecheng1996lzg <lens96@foxmail.com>
2021-12-10 15:18:56 +0800

    可以屏蔽20分钟的弹窗验证了

---
+++ js/content-script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // 注入自定义JS
+  injectCustomJs();
+});
+function injectCustomJs(jsPath)
+{
+  jsPath = jsPath || 'js/inject.js';
+  var temp = document.createElement('script');
+  temp.setAttribute('type', 'text/javascript');
+  // 获得的地址类似：chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/js/inject.js
+  temp.src = chrome.extension.getURL(jsPath);
+  temp.onload = function()
+  {
+    // 放在页面不好看，执行完后移除掉
+    this.parentNode.removeChild(this);
+  };
+  document.head.appendChild(temp);
+}
---
+++ js/inject.js
@@ -0,0 +1,3 @@
+function isPopWin(playTime){
+  return false;
+}
